aeb0bd5e1c736b7d1bd8ee81608450d73fbbe508
Roottoaccess <network123crab@gmail.com>
2025-02-18 14:31:15 +0530

    Shell Scripting for DEvOPs

---
+++ another_important.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+word="awsome"
+echo "Linux is $word"
+echo "Video games are $word"
+echo "Sunny days are $word"
+
+
---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# var_name="server_is_in_holding" # Creating the variable !!
+# echo $var_name # Displaying the variable mesage !!
+
+# Creating some new operations !!
+my_name="Petter"
+my_age="24"
+echo Name: $my_name,  Age: $my_age
