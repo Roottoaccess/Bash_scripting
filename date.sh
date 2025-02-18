7dcde0dcf8cfd0c16f17cf0000215de072425809
Roottoaccess <network123crab@gmail.com>
2025-02-18 14:44:35 +0530

    Bash Scripting

--- date.sh
+++ date.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 # Making this script better !!
+
+
+name="Macinthosh Red Handed"
+now=$(date)
+    # displaying the mesage for this system !!
+echo "Machine Name: $name"
+echo "Machine Time right now: $now"
+
+echo "System successfully executed :)"
+
+
+echo "Your user name is $USER"e
